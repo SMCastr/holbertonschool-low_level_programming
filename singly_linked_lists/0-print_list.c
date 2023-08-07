f48b4a7e50116f232f915f4ab99ed524203c6d64
SMCastr <mariesweetycastro@gmail.com>
2023-08-07 17:57:24 +0000

    fix

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#ifndef MAIN_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @h: header of list
+ *  
+ * Return: always (0).
+ */
+
+size_t print_list(const list_t *h);
+{
+	int num;
+
+	for (num = 0; num != NULL; num++)
+	{        if (h ->str == NULL)
+		printf("[0],(nil)\n");
+		else ("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (num);
+}
