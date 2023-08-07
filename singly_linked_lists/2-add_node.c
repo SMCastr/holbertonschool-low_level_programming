839680f0b7119b670dcafd8693c324215e451674
SMCastr <mariesweetycastro@gmail.com>
2023-08-07 21:49:02 +0000

    Added file task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_node - ...
+ * @head: ...
+ * @str: ...
+ * Return: ...
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp;
+
+	if (head != NULL && str != NULL)
+	{
+		tmp = malloc(sizeof(list_t));
+		if (tmp == NULL)
+			return (NULL);
+		tmp->str = strdup(str);
+		tmp->len = strlen(str);
+		tmp->next = *head;
+		*head = temp;
+	}
+	return (0);
+}
