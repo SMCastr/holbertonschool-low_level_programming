bfb294e1304b0509058e265a8f0879e7ac8cf632
SMCastr <mariesweetycastro@gmail.com>
2023-08-07 22:01:03 +0000

    Added file task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - ...
+ * @head: ...
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+	free(head);
+}
