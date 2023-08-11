da16f122943391182503ab2082719314596e7f73
SMCastr <mariesweetycastro@gmail.com>
2023-08-10 22:20:54 -0400

    betty

--- file_io/main.h
+++ file_io/main.h
@@ -1,14 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 #endif
