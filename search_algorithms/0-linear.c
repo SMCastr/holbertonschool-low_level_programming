af258d405d36f959e46d64ea1fb8e0fbd8a8309d
SMCastr <mariesweetycastro@gmail.com>
2023-09-20 14:12:25 -0400

    declarations

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,12 +10,15 @@
  *
  * Return: The first index where 'value' is located, or -1 if not found.
  */
-int linear_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)	
 {
+	int array_size = (int)value, 
+		i;
+
 	if (array == NULL)
 		return (-1);
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
