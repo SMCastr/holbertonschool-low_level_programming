ec5667c980bbdd6e9b775789a65869052fbe3a28
SMCastr <mariesweetycastro@gmail.com>
2023-09-23 00:19:45 -0400

    sort

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,12 +1,13 @@
 #ifndef SEARCH_ALGO_H
 #define SEARCH_ALGO_H
 
-typedef struct {
-    int *arr;
-    int size;
-} Array;    /* Array struct */
+typedef struct
+{
+	int *arr;
+	int size;
+} Array;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
-#endif /* SEARCH_ALGO_H */
+#endif
